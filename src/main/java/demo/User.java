d681642978bccdefd2394fcbf900849dda6799e5
dmmasxmf <15738669005@139.com>
2018-08-24 19:44:16 +0800

    增加了用户类，最后一次了

---
+++ src/main/java/demo/User.java
@@ -0,0 +1,34 @@
+package demo;
+
+public class User {
+
+    private int age;
+
+    private String username;
+
+    private String password;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
